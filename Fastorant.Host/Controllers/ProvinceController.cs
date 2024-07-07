d23c93fcfed3ec025548c7007a788805850e7e21
Mharis13 <137278432+Mharis13@users.noreply.github.com>
2024-07-07 22:38:11 +0200

    BACKEND:  PROVINCE.CONTROLLER ADD ENPOINT GET ALL BACKEND:  PROVINCE.CONTROLLER ADD ENPOINT GET BY ID

--- Fastorant.Host/Controllers/ProvinceController.cs
+++ Fastorant.Host/Controllers/ProvinceController.cs
@@ -1,4 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Fastorant.Infrastructure;
+using Fastorant.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Fastorant.Host.Controllers
 {
@@ -6,10 +14,33 @@ namespace Fastorant.Host.Controllers
     [Route("api/[controller]")]
     public class ProvinceController : ControllerBase
     {
+        private readonly FastorantContext _context;
+
+        public ProvinceController(FastorantContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            return Ok("Hola");
+            var provinces = await _context.Provinces.ToListAsync();
+            return Ok(provinces);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(long id)
+        {
+            var province = await _context.Provinces.FindAsync(id);
+
+            if (province == null)
+            {
+                NotFound(id); 
+                return BadRequest();
+
+            }
+            return Ok(province);
+        }
+
     }
 }
